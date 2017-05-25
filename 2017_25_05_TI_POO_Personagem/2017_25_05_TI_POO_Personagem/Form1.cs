614121c37997bd6a2d7474382ef4138f2b9c3d6b
Pedrohf360 <pedrohf360@gmail.com>
2017-05-25 18:41:07 -0300

    Início da implementação dos personagens: criação da relação de classes e pré-definição dos métodos.

---
+++ 2017_25_05_TI_POO_Personagem/2017_25_05_TI_POO_Personagem/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2017_25_05_TI_POO_Personagem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ 2017_25_05_TI_POO_Personagem/2017_25_05_TI_POO_Personagem/Personagem.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017_25_05_TI_POO_Personagem
+{
+    public abstract class Personagem
+    {
+        protected int velocidade;
+        protected Boolean ultrapassaLimite;
+
+
+        /// <summary>
+        /// Determina a velocidade do personagem
+        /// </summary>
+        /// <param name="velocidade">Valor da velocidade.</param>
+        public void SetVelocidade
+        {
+            set { this. = value;}
+        }
+        
+
+        public bool VerifLimites()
+        {
+
+        }
+
+        public void SetDirecao
+        {
+            set {this. = value;}
+        }
+
+        public void Teleporte()
+        {
+
+        }
+
+        public void Mover()
+        {
+
+        }
+    }
+}
